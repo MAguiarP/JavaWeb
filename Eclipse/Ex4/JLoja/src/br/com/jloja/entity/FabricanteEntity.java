d388042d9cba9c96f26065f75a97244f130be176
MAguiarP <matheusdeaguiar@live.com>
2022-08-03 15:16:44 -0300

    Ex4

---
+++ Eclipse/Ex4/JLoja/src/br/com/jloja/entity/FabricanteEntity.java
@@ -0,0 +1,41 @@
+package br.com.jloja.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="fabricante")
+public class FabricanteEntity {
+	
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy="increment")
+	
+	@Column(name = "idfabricante")
+	private Long idfabricante;
+	
+	@Column(length = 60, nullable = false)
+	private String descricao;
+
+	public Long getIdfabricante() {
+		return idfabricante;
+	}
+
+	public void setIdfabricante(Long idfabricante) {
+		this.idfabricante = idfabricante;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	}
