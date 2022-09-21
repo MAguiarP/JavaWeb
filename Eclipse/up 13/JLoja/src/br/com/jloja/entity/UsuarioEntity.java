861faa3db214c5acee4460cd23eb1fdda30ea3e7
MAguiarP <matheusdeaguiar@live.com>
2022-09-21 10:58:28 -0300

    Adds

---
+++ Eclipse/up 13/JLoja/src/br/com/jloja/entity/UsuarioEntity.java
@@ -0,0 +1,11 @@
+package br.com.jloja.entity;
+
+public class UsuarioEntity {
+
+private Long idusuario;
+private String nome;
+private String login;
+private String senha;
+private char situacao;
+
+}
